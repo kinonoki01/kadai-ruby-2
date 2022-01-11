d3740da4680f4a8cd11e9d1e9745945eef995824
daichi.maruyama <kinonoki01@gmail.com>
2022-01-11 12:18:07 +0000

    メソッド名の修正、インデントを半角スペース*2へ修正の対応を行った

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,16 @@
-def FizzBuzz(num)
-    if num % 15 == 0
-        "FizzBuzz"
-    elsif num % 5 == 0
-        "Buzz"
-    elsif num % 3 == 0
-        "Fizz"
-    else
-        num.to_s
-    end
+def fizzbuzz(num)
+  if num % 15 == 0
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  else
+    num.to_s
+  end
 end
 
 num_max = 100
 (1..num_max).each do |num|
-    puts FizzBuzz(num)
+  puts fizzbuzz(num)
 end
